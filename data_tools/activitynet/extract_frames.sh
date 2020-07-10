16b88c079dbde15b8fad0a1319edaa3873e5899c
Victor Escorcia <escorciav@gmail.com>
2020-07-10 22:15:14 +0100

    [data_tools] Minor fix activitynet flow extraction
    Silly mistake of referring to wrong subset 😓.

--- data_tools/activitynet/extract_frames.sh
+++ data_tools/activitynet/extract_frames.sh
@@ -13,7 +13,7 @@ echo "Raw frames (RGB and tv-l1) Generated for v1-2 test"
 python build_rawframes.py ../data/activitynet/v1-3/train_val ../data/activitynet/rawframes --level 2 --flow_type tvl1 --ext all --ignore_level_out/ | tee activitynet/log_v1-3_train_val.txt &&
 echo "Raw frames (RGB and tv-l1) Generated for v1-3 train_val"
 
-python build_rawframes.py ../data/activitynet/v1-3/val ../data/activitynet/rawframes/ --level 2 --flow_type tvl1 --ext all --ignore_level_out | tee activitynet/log_v1-3_test.txt &&
+python build_rawframes.py ../data/activitynet/v1-3/test ../data/activitynet/rawframes/ --level 2 --flow_type tvl1 --ext all --ignore_level_out | tee activitynet/log_v1-3_test.txt &&
 echo "Raw frames (RGB and tv-l1) Generated for v1-3 test"
 
 cd activitynet/
